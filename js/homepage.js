b8c7f85b8099379c7173fb67190bf2131421b2d0
William Seiti Mizuta <william.mizuta@gmail.com>
2012-12-07 03:57:12 -0200

    Showing current section

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function($) {
 	$('.menu').fixHeader();
+
+	$(window).bind('scroll', function() {
+		$('.menu-item').removeClass('active');
+		var sectionsAbove = $('.section').map(function(index, element){
+			return {
+				element: element,
+				offset: $(element).offset().top - $(window).scrollTop() - $('.menu').outerHeight()
+			};
+		}).filter(function(i) {
+			return this.offset <= 1;
+		});
+		if (sectionsAbove.length > 0) {
+			var currentSection = sectionsAbove.last()[0].element;
+			$('.menu-item a[href$=' + $(currentSection).attr('id') + ']').parent().addClass('active');
+		}
+	});
 });
